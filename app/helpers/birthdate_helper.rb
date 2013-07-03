4f92b2d3d9ca0d7e926c7d6a786766c24dc01c91
Ahmed Omran <ahmedomran@Ahmeds-MacBook-Air.local>
2013-07-03 11:24:56 -0400

    playing around with time difference helper method

--- app/helpers/birthdate_helper.rb
+++ app/helpers/birthdate_helper.rb
@@ -1,2 +1,15 @@
 module BirthdateHelper
+
+	def get_time_diff(time1, time2)
+		diff_in_seconds = (time1 - time2).floor
+		components = []
+		intervals = [:years]
+		
+		intervals.each do |interval|
+			component = diff_in_seconds / 1.send(interval)
+			diff_in_seconds -= component
+			components << component
+		end
+		components
+	end
 end
