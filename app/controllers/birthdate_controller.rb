b2e2d946e99aefe9013b3b0ee4d160a50817f2e2
Ahmed O <aomra015@gmail.com>
2013-07-03 14:09:45 -0400

    fixed date diff accuracy

--- app/controllers/birthdate_controller.rb
+++ app/controllers/birthdate_controller.rb
@@ -2,10 +2,11 @@ class BirthdateController < ApplicationController
   def index
   	@date_today = Date.current
   	@birth_date = Date.new(1987,4,5)
+  	@date_diff = Time.diff(@birth_date, @date_today, '%y, %M, %w, and %d')
   	@days_since_birthdate = (@date_today - @birth_date).to_i
   end
 
   def calc
-  	@birth_date = "#{params[:birth_date][:year]}-#{params[:birth_date][:month]}-#{params[:birth_date][:day]}".to_datetime
+  	@birth_date = "#{params[:birth_date][:year]}-#{params[:birth_date][:month]}-#{params[:birth_date][:day]}".to_date
   end
 end
