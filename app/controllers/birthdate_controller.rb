f28044cca9a1f3d1d8d13d156cfccd6ab164708a
vagrant <vagrant@precise64.(none)>
2013-07-05 17:26:35 +0000

    year change in birthdate

--- app/controllers/birthdate_controller.rb
+++ app/controllers/birthdate_controller.rb
@@ -1,7 +1,7 @@
 class BirthdateController < ApplicationController
   def index
   	@date_today = Date.current
-  	@birth_date = Date.new(1987,4,5)
+  	@birth_date = Date.new(1901,4,5)
   	@date_diff = Time.diff(@birth_date, @date_today, '%y, %M, %w, and %d')
   	@days_since_birthdate = (@date_today - @birth_date).to_i
   end
